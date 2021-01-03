aa4ea5d3f8651695dbae7b23e48355768a9794fb
gantian127 <jamy127@foxmail.com>
2021-01-02 19:58:53 -0700

    update cli for new parameters

--- soilgrids/cli.py
+++ soilgrids/cli.py
@@ -29,13 +29,50 @@ from soilgrids import SoilGrids
     help="Bounding box for data download . "
          "Values are based on the specified crs in a sequence of west, south, east, north separated by comma.",
 )
+@click.option(
+    "--resx",
+    required=False,
+    default=250,
+    type=float,
+    help="Pixel resolution in x direction. "
+         "Required when crs is set as a projection coordinate system. Default value set as 250 (m)",
+)
+@click.option(
+    "--resy",
+    required=False,
+    default=250,
+    type=float,
+    help="Pixel resolution in y direction. "
+         "Required when crs is set as a projection coordinate system. Default value set as 250 (m)",
+)
+@click.option(
+    "--width",
+    required=False,
+    default=None,
+    type=float,
+    help="Width in pixels for data download. Required when crs is set as a geographic coordinate system. ",
+)
+@click.option(
+    "--height",
+    required=False,
+    default=None,
+    type=float,
+    help="height in pixels for data download. Required when crs is set as a geographic coordinate system. ",
+)
+@click.option(
+    "--response_crs",
+    required=False,
+    default=None,
+    help="Coordinate system code for the GeoTiff file.",
+)
 @click.argument(
     'output',
     type=click.Path(exists=False)
 )
-def main(service_id, coverage_id, crs, bbox, output):
+def main(service_id, coverage_id, crs, bbox, resx, resy, width, height, response_crs, output):
     west, south, east, north = list(map(float, bbox.split(',')))
     SoilGrids().get_coverage_data(service_id=service_id, coverage_id=coverage_id, crs=crs,
-                                  west=west, south=south, east=east, north=north, output=output)
+                                  west=west, south=south, east=east, north=north, output=output,
+                                  resx=resx, resy=resy, width=width, height=height, response_crs=response_crs)
     if os.path.isfile(output):
         print('Done')
